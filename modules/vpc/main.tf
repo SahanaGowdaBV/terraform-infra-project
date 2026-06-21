ce9af78308ef2914b98c31a2b07e2653c9190995
SahanaGowdaBV <111375256+SahanaGowdaBV@users.noreply.github.com>
2026-06-21 19:01:01 +0530

    Add VPC, subnets, and internet gateway resources

---
+++ modules/vpc/main.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  tags = { Name = var.name }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
