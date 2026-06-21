f5d91fdd5f2740b29fbeeef05b762edbb843c7cd
SahanaGowdaBV <111375256+SahanaGowdaBV@users.noreply.github.com>
2026-06-21 19:04:15 +0530

    Add EC2 module variables for region, AMI, and instance type

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "ami"           { type = string }
+variable "instance_type" { type = string }
+variable "name"          { type = string }
