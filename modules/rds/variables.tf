b36e8eca960398e69271198c907ee81dfeb2a172
SahanaGowdaBV <111375256+SahanaGowdaBV@users.noreply.github.com>
2026-06-21 19:06:54 +0530

    Add variables for RDS module configuration

---
+++ modules/rds/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "db_name"  { type = string }
+variable "username" { type = string }
+variable "password" { type = string }
